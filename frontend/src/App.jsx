fe56c9f5af582e1adbda6aef523052c26186b56d
cpchsh <cpchsh319@gmail.com>
2025-01-05 21:26:52 +0800

    將月預測資料放進db 9-> navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,43 +1,78 @@
 // App.jsx
-import React from "react";
+import React, { useState } from "react";
 import ChartContainer from "./containers/ChartContainer";
 import MonthChartContainer from "./containers/MonthChartContainer";
 
 function App() {
+  const [activeTab, setActiveTab] = useState("daily");
+
+  // Tab 切換 handle
+  const handleTabChange = (tabName) => {
+    setActiveTab(tabName);
+  };
   return (
     <div className="container-fluid">
       <h1 className="text-center">Luboil Data Visualization</h1>
-      {/* 左右圖 (日) - 不含 futureData */}
-      <div className="row">
-        <div className="col-6">
-          <ChartContainer includeFutureData={false} />
-        </div>
-        <div className="col-6">
-          <ChartContainer includeFutureData={false} />
-        </div>
-      </div>
-      {/* 下圖 (日) - 含 futureData */}
-      <div className="row mt-5">
-        <div className="col-12">
-          <ChartContainer includeFutureData={true} />
-        </div>
-      </div>
+      {/* Bootstrap NavTabs */}
+      <ul className="nav nav-tabs">
+        <li className="nav-item">
+          <button
+            className={`nav-link ${activeTab === "daily" ? "active" : ""}`}
+            onClick={() => handleTabChange("daily")}
+          >
+            日資料(Daily)
+          </button>
+        </li>
+        <li className="nav-item">
+          <button
+            className={`nav-link ${activeTab === "monthly" ? "active" : ""}`}
+            onClick={() => handleTabChange("monthly")}
+          >
+            月資料(Monthly)
+          </button>
+        </li>
+      </ul>
 
-      {/* 左右圖 (月) - 不含 futureData */}
-      <div className="row">
-        <div className="col-6">
-          <MonthChartContainer includeFutureData={false} />
+      {/* 依 activeTab 顯示內容 */}
+      {activeTab === "daily" && (
+        <div className="mt-3">
+          {/* 左右圖 (日) - 不含 futureData */}
+          <div className="row">
+            <div className="col-6">
+              <ChartContainer includeFutureData={false} />
+            </div>
+            <div className="col-6">
+              <ChartContainer includeFutureData={false} />
+            </div>
+          </div>
+          {/* 下圖 (日) - 含 futureData */}
+          <div className="row mt-5">
+            <div className="col-12">
+              <ChartContainer includeFutureData={true} />
+            </div>
+          </div>
         </div>
-        <div className="col-6">
-          <MonthChartContainer includeFutureData={false} />
+      )}
+
+      {activeTab === "monthly" && (
+        <div className="mt-3">
+          {/* 左右圖 (月) - 不含 futureData */}
+          <div className="row">
+            <div className="col-6">
+              <MonthChartContainer includeFutureData={false} />
+            </div>
+            <div className="col-6">
+              <MonthChartContainer includeFutureData={false} />
+            </div>
+          </div>
+          {/* 下圖 (月) - 含 futureData */}
+          <div className="row mt-5">
+            <div className="col-12">
+              <MonthChartContainer includeFutureData={true} />
+            </div>
+          </div>
         </div>
-      </div>
-      {/* 下圖 (月) - 含 futureData */}
-      <div className="row mt-5">
-        <div className="col-12">
-          <MonthChartContainer includeFutureData={true} />
-        </div>
-      </div>
+      )}
     </div>
   );
 }
